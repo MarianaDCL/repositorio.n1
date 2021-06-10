67a49a4d37328e70ce766cc7e3198643fb8f2c7b
Mariana Lima <marianadecamposlima.ar@gmail.com>
2021-06-10 13:55:24 -0300

    novosarquivos

---
+++ exercícios/exer2.js
@@ -0,0 +1,17 @@
+//ex 1
+let status = true;
+if (status = true) {
+    console.log('o valor é vedadeiro');
+}
+else{
+    console.log('o valor é falso');
+}
+//ex 2
+let linguagem = 'linguagem HTML';
+
+if(linguagem == 'javascript'){
+    console.log('estou aprendendo');
+}
+else{
+    console.log('aprenderei mais tarde');
+}
---
+++ exercícios/exer3.js
@@ -0,0 +1,17 @@
+//ex 1
+for (let papagaio = 1;papagaio <=5;papagaio++){
+    console.log('olá mundo');
+}
+//ex 2
+for (let numeros = 1;numeros <= 10;numeros++){
+    for (let tabuada = 0;tabuada <= 10;tabuada++){
+    console.log(tabuada*numeros);}
+}
+//ex 3
+for (let impares = 0;impares <= 10;impares++){
+    let resultado = impares%2
+    if(resultado==1){
+        console.log(impares)
+    }
+}
+
