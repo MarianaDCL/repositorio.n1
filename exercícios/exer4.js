8b6d342beb90b7e4a62c5be02cb12b096965a264
Mariana Lima <marianadecamposlima.ar@gmail.com>
2021-06-14 13:11:28 -0300

    novo ex

---
+++ exercícios/exer4.js
@@ -0,0 +1,21 @@
+//ex 1
+/*function print(mensagem){
+    console.log(mensagem);
+}
+print('Olá, Bom Dia');*/
+
+let print = mensagem =>console.log(mensagem);
+print('Olá, Bom Dia');
+
+/*function soma(n1,n2){ 
+    return n1+n2
+}
+console.log(soma(10,10));*/
+let soma = (n1,n2) => n1+n2;
+console.log(soma(10,10));
+
+//ex 2 ????
+const andando ='o carro está andando';
+const parando ='o carro parou.';
+console.log(andando+', '+ parando);
+//???
